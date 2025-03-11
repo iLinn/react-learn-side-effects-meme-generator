7b758e0897bafd0071537429116a1e691bb215ef
iLinn <11552398+iLinn@users.noreply.github.com>
2025-03-11 16:19:16 +0200

    Added IMG api fetch request; updated input values

--- src/components/main-component/Main.tsx
+++ src/components/main-component/Main.tsx
@@ -1,6 +1,54 @@
+import { useState, useEffect } from 'react';
 import './Main.css';
 
+export interface MemeData {
+  imageUrl: string;
+  topText: string;
+  bottomText: string;
+}
+
+export interface MemeApi {
+  url: string;
+  id: string;
+  name: string;
+}
+
+export interface MemeResponse {
+  success: boolean;
+  data: {
+    memes: MemeApi[];
+  };
+}
+
 export default function Main() {
+  const defaultMemeData: MemeData = {
+    imageUrl: 'http://i.imgflip.com/1bij.jpg',
+    topText: 'One does not simply',
+    bottomText: 'Walk into Mordor',
+  }
+  const [memeData, setMemeData] = useState<MemeData>(defaultMemeData);
+  const MEME_URL = 'https://api.imgflip.com/get_memes';
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    console.log(name, value);
+    setMemeData((prev) => ({ ...prev, [name]: value }));
+  }
+
+  useEffect(() => {
+    fetch(MEME_URL)
+      .then((response) => response.json())
+      .then((data: MemeResponse) => {
+        const { memes } = data.data;
+        const randomIndex = Math.floor(Math.random() * memes.length);
+        const randomMeme = memes?.[randomIndex] ? memes[randomIndex] : 
+          memes[0] ? memes[0] : { url: defaultMemeData.imageUrl };
+        setMemeData((prev) => ({ ...prev, imageUrl: randomMeme.url }));
+    }).catch((error) => {
+      console.error('Error fetching meme data:', error);
+    });
+  }, []);
+
   return (
       <main>
           <div className="form">
@@ -9,6 +57,8 @@ export default function Main() {
                       type="text"
                       placeholder="One does not simply"
                       name="topText"
+                      onChange={handleChange}
+                      value={memeData?.topText}
                   />
               </label>
 
@@ -17,14 +67,16 @@ export default function Main() {
                       type="text"
                       placeholder="Walk into Mordor"
                       name="bottomText"
+                      onChange={handleChange}
+                      value={memeData?.bottomText}
                   />
               </label>
               <button>Get a new meme image 🖼</button>
           </div>
           <div className="meme">
-              <img src="http://i.imgflip.com/1bij.jpg" />
-              <span className="top">One does not simply</span>
-              <span className="bottom">Walk into Mordor</span>
+              {memeData?.imageUrl && <img src={memeData?.imageUrl} alt="Meme picture"/>}
+              <span className="top">{memeData?.topText}</span>
+              <span className="bottom">{memeData?.bottomText}</span>
           </div>
       </main>
   )
